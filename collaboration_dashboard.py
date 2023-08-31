fb926cbd300c27d383bd6c324149ef0e4c10ba99
Shauny <14363407+shauny2015@users.noreply.github.com>
2023-08-31 11:46:56 +0100

    Update collaboration_dashboard.py
    Using browser session storage

--- collaboration_dashboard.py
+++ collaboration_dashboard.py
@@ -4,21 +4,26 @@ import streamlit as st
 st.title("Idea Submission Form")
 st.header('Study day ideas')
 
-#Ideas list
-ideasList = []
+
+#init session state
+if "ideasList" not in st.session_state:
+    st.session_state.ideasList = []
 
 # Input fields
 userName = st.text_input("Your Name", "")
-userIdeas = st.text_area("Your Ideas", "")
+userEmail = st.text_input("Your email","")
+userIdea = st.text_area("Your Ideas", "")
 
 # Submit button
 if st.button("Submit"):
-    if userName and userIdeas:
+    if userName and userIdea:
         st.success("Thank you for submitting your idea!")
-        ideasList.append(f"{userIdeas}")
+        st.session_state.ideasList.append(f"{userName} : {userIdea}")
     else:
         st.warning("Please fill in both your name and ideas.")
  #display ideas back to the user
 
-if ideasList:
-     st.listbox(ideasList)
+if st.session_state.ideasList:
+    st.title("Ideas")
+    for idea in st.session_state.ideasList:
+        st.write(idea)
